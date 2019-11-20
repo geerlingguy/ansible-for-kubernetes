10132907973aeb513cae813f5ba85d055386a62f
Jeff Geerling <geerlingguy@mac.com>
2019-11-19 21:28:49 -0600

    Automate the Hello Go app build and deployment in Minikube.

---
+++ hello-go-automation/README.md
@@ -0,0 +1,25 @@
+# Automation for the Hello World Go app
+
+This folder contains the Ansible playbook used to automate all the manual steps in building and deploying the example [Hello Go app](../hello-go) located elsewhere in this repository.
+
+## Usage
+
+The Ansible playbook assumes you have the following installed on your workstation:
+
+  - [Minikube](https://kubernetes.io/docs/tasks/tools/install-minikube/)
+  - [Ansible](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html)
+
+You will also need to install a Python dependencies via Python Pip:
+
+    pip3 install openshift
+
+Once they are installed, you can run the playbook with:
+
+    ansible-playbook main.yml
+
+Once you're finished testing the Hello Go app in the Kubernetes cluster, you can clean it up by deleting the entire Minikube cluster with `minikube delete`, or you can run the following commands to delete just the necessary resources and leave the Minikube cluster running:
+
+    kubectl delete service hello-go
+    kubectl delete deployment hello-go
+
+The above commands assume you have `kubectl` installed.
---
+++ tests/hello-go-automation.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Tests for hello-go-automation playbook.
+cd hello-go-automation
+
+export CHANGE_MINIKUBE_NONE_USER=true
+export K8s_VERSION="v1.16.2"
+
+# Install kubectl.
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+# Install minikube.
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+# Install Ansible.
+pip3 install ansible openshift
+
+# Start minikube (without the VM driver).
+sudo minikube start --vm-driver=none
+minikube update-context
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
+  until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+
+# Once Minikube is started, run the Ansible playbook.
+ansible-playbook main.yml
+
+# Get the Hello Go URL from Minikube.
+HELLO_GO_URL=$(minikube service hello-go --url=true)
+
+# Test the response with curl.
+if curl -s $HELLO_GO_URL | grep "Hello, you requested: /"; then
+    echo "Received the correct response."
+else
+    echo "Did not receive the correct response."
+    exit 1
+fi
