d6330b2164fd9c1dc00047aeb6ce2eb43000bfa5
Jeff Geerling <geerlingguy@mac.com>
2019-11-19 21:45:37 -0600

    Python was installed but not Pip3. So there.

--- tests/hello-go-automation.sh
+++ tests/hello-go-automation.sh
@@ -15,7 +15,8 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 # Install Ansible.
-pip install ansible openshift
+apt-get install -y python3-pip
+pip3 install ansible openshift
 
 # Start minikube (without the VM driver).
 sudo minikube start --vm-driver=none
