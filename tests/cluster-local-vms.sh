418274ba4a621bde1b5396b4229ed850dfff126f
Jeff Geerling <geerlingguy@mac.com>
2019-12-14 15:02:05 -0600

    Install docker manually inside controller container.

--- tests/cluster-local-vms.sh
+++ tests/cluster-local-vms.sh
@@ -15,6 +15,15 @@ ansible-lint main.yml
 which docker
 docker-compose up -d
 
+# Install Docker inside the controller container.
+docker-compose exec controller bash -c "\
+  apt update && \
+  apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
+  apt update && \
+  apt install -y docker-ce"
+
 # Execute the Cluster playbook inside the controller container.
 docker-compose exec controller bash -c "cd /opt/ansible && ansible-galaxy install -r requirements.yml"
 docker-compose exec controller bash -c "cd /opt/ansible && ansible-playbook -i inventory main.yml"
