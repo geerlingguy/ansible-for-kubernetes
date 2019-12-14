ec452678915ecfebcc137c1beb01598c94563712
Jeff Geerling <geerlingguy@mac.com>
2019-12-14 15:11:45 -0600

    Specify buster release for Docker install.

--- tests/cluster-local-vms.sh
+++ tests/cluster-local-vms.sh
@@ -20,7 +20,7 @@ docker-compose exec controller bash -c "\
   apt update && \
   apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
-  add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\" && \
+  add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian buster stable\" && \
   apt update && \
   apt install -y docker-ce"
 
