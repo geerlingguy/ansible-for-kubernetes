09a450d22cfe56b77deab543c20d80421f164a26
Jeff Geerling <geerlingguy@mac.com>
2019-12-15 14:56:08 -0600

    More tests.

--- tests/test-dns.sh
+++ tests/test-dns.sh
@@ -11,3 +11,14 @@ docker exec curltest bash -c "apt update && apt install -y curl"
 
 # Test a curl command in that docker container.
 docker exec curltest bash -c "curl --head https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+
+# Test convoluted curl command in that docker container.
+docker exec -i curltest /bin/sh -c "sudo -H -S -n -u root /bin/sh -c 'curl --head https://packages.cloud.google.com/apt/doc/apt-key.gpg'"
+
+# Kill that docker container.
+docker rm -f curltest
+
+# Try running docker-compose-based things and see if DNS resolves.
+cd cluster-local-vms
+docker-compose up -d
+docker-compose exec kube2 bash -c "curl --head https://packages.cloud.google.com/apt/doc/apt-key.gpg"
