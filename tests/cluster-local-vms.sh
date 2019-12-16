13c99c7de108d2b3e78c35dff6d0ba63bdfa3efc
Jeff Geerling <geerlingguy@mac.com>
2019-12-16 12:12:24 -0600

    Attempt force-mounting a resolv.conf file in the kube containers.

--- tests/cluster-local-vms.sh
+++ tests/cluster-local-vms.sh
@@ -7,14 +7,21 @@ cd cluster-local-vms
 
 # If on Travis CI, update Docker's configuration.
 if [ "$TRAVIS" == "true" ]; then
+  # Create temp dirs for overlayfs.
   mkdir -p /tmp/docker/kube1
   mkdir -p /tmp/docker/kube2
   mkdir -p /tmp/docker/kube3
+
+  # Write a custom resolv.conf file to be mounted.
+  echo 'nameserver 8.8.8.8' | sudo tee /tmp/docker/resolv.conf
+
+  # Write Docker daemon config.
   echo '{
     "experimental": true,
-    "storage-driver": "overlay2",
-    "dns": ["1.1.1.1", "1.0.0.1"]
+    "storage-driver": "overlay2"
   }' | sudo tee /etc/docker/daemon.json
+
+  # Restart Docker.
   sudo service docker restart
 fi
 
@@ -34,8 +41,4 @@ docker-compose up -d
 ansible-playbook -i inventory-docker main.yml || true
 
 # Get some debug info.
-docker-compose exec kube1 bash -c "systemctl status docker.service --no-pager --full"
-docker-compose exec kube2 bash -c "systemctl status docker.service --no-pager --full"
-docker-compose exec kube1 bash -c "journalctl --no-pager -u docker"
-docker-compose exec kube2 bash -c "journalctl --no-pager -u docker"
 docker-compose exec kube1 bash -c "journalctl --no-pager -u kubelet"
