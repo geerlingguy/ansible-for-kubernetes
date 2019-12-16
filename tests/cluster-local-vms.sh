0f509dd9795f913654e6a1a8a4ed0f9706f4b57e
Jeff Geerling <geerlingguy@mac.com>
2019-12-15 22:09:24 -0600

    Forgot to add changes to actual setup script.

--- tests/cluster-local-vms.sh
+++ tests/cluster-local-vms.sh
@@ -5,6 +5,16 @@ set -e
 
 cd cluster-local-vms
 
+# If on Travis CI, update Docker's configuration.
+if [ "$TRAVIS" == "true" ]; then
+  mkdir /tmp/docker
+  echo '{
+    "experimental": true,
+    "storage-driver": "overlay2"
+  }' | sudo tee /etc/docker/daemon.json
+  sudo service docker restart
+fi
+
 # Install Ansible and required dependencies.
 pip install ansible ansible-lint
 
