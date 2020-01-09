b9b57d491c3dc9335b316f93769d83f4487c9c48
Jeff Geerling <geerlingguy@mac.com>
2020-01-08 19:13:07 -0600

    Issue #15: Better formatting for EKS tests.

--- tests/cluster-aws-eks.sh
+++ tests/cluster-aws-eks.sh
@@ -6,7 +6,9 @@ set -e
 cd cluster-aws-eks
 
 # Install AWS CLI.
+travis_fold start "install.pip.deps"
 pip3 install awscli yamllint cfn-lint ansible-lint ansible openshift
+travis_fold end "install.pip.deps"
 
 # Export AWS vars.
 export AWS_DEFAULT_REGION=us-east-1
@@ -28,7 +30,7 @@ echo "Linting playbooks with ansible-lint..."
 ansible-lint $playbooks
 
 # Prepare a Kind cluster.
-echo "Preparing Kind cluster..."
+travis_fold start "prepare.kind.cluster"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
@@ -37,8 +39,10 @@ chmod +x kind
 sudo mv kind /usr/local/bin/
 export KUBECONFIG="${HOME}/.kube/kind-config-test"
 kind create cluster --name=test --quiet
+travis_fold end "prepare.kind.cluster"
 
 # Test Wordpress manifests in Kind cluster.
+ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 \
 ansible-playbook -i inventory deploy.yml \
   -e "k8s_kubeconfig=$KUBECONFIG" \
   -e "aws_environment=false" \
