90dea26179be97832182f8ab73baf412f96f8571
Jeff Geerling <geerlingguy@mac.com>
2020-03-28 23:05:18 -0500

    Issue #36: Use sudo for kind and kubectl downloads.

--- tests/testing-molecule-kind.sh
+++ tests/testing-molecule-kind.sh
@@ -9,12 +9,12 @@ cd testing-molecule-kind
 pip install ansible ansible-lint molecule
 
 # Install Kind.
-curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/"$KIND_VERSION"/kind-linux-amd64
-chmod +x /usr/local/bin/kind
+sudo curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/"$KIND_VERSION"/kind-linux-amd64
+sudo chmod +x /usr/local/bin/kind
 
 # Install kubectl.
-curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x /usr/local/bin/kubectl
+sudo curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+sudo chmod +x /usr/local/bin/kubectl
 
 # Test with Molecule.
 molecule test
